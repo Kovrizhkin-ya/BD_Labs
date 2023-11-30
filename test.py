ba4adbfb75f6e65f9cd21ac32eb36bfe104aeaac
Kovrizhkin-ya <kovrizhkinaleksandr95@gmail.com>
2023-11-30 19:33:24 +0200

    Delete test.py
    delet test

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-print(load_dotenv("C:/Users/38097\Desktop/Lab_1/config.env"))
-
-
-print(os.getenv("DB_USERNAME"))
